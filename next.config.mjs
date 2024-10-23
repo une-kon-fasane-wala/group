fb6de87000d9a10416a46597cc85108cdd1c38de
une-kon-fasane-wala <une-kon-fasane-wala@gmail.com>
2024-10-23 21:53:26 +0530

    Attempt re-deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 const nextConfig = {
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        pathname: '/uc**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        pathname: '/file/d/**',
+      },
+    ],
   },
 };
 
